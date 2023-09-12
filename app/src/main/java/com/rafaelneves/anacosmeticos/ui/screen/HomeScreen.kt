af8f97f7b39defbbb4e60f6b4c102888eaef9f4b
Rafael Neves <rafaelneves02@gmail.com>
2023-09-12 14:50:57 -0300

    Created HomeScreen

---
+++ app/src/main/java/com/rafaelneves/anacosmeticos/ui/screen/HomeScreen.kt
@@ -0,0 +1,77 @@
+package com.rafaelneves.anacosmeticos.ui.screen
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.rafaelneves.anacosmeticos.R
+import com.rafaelneves.anacosmeticos.ui.theme.AnaCosmeticosTheme
+import com.rafaelneves.anacosmeticos.ui.widget.CardHomeScreen
+
+
+data class ItemHomeScreen(
+    @StringRes val title: Int,
+    val icon: Int
+)
+
+val listHomeScreen = listOf(
+    ItemHomeScreen(title = R.string.new_shipping, icon = R.drawable.ic_create_shipping),
+    ItemHomeScreen(title = R.string.shipping, icon = R.drawable.ic_shipping),
+    ItemHomeScreen(title = R.string.stock, icon = R.drawable.ic_stock),
+    ItemHomeScreen(title = R.string.new_product, icon = R.drawable.ic_add)
+)
+
+@Composable
+fun HomeScreen() {
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        LazyColumn(
+            modifier = Modifier
+                .background(Color.White)
+                .fillMaxSize(),
+
+            ) {
+
+            item {
+                Spacer(modifier = Modifier.padding(16.dp))
+            }
+
+            items(listHomeScreen) { item ->
+                CardHomeScreen(
+                    icon = painterResource(id = item.icon),
+                    text = stringResource(id = item.title),
+                    onClick = {
+                        when (item.title) {
+                            R.string.new_shipping -> {}
+                            R.string.shipping -> {}
+                            R.string.stock -> {}
+                            R.string.new_product -> {}
+                        }
+                    }
+                )
+            }
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun HomeScreenPreview() {
+    AnaCosmeticosTheme {
+        HomeScreen()
+    }
+}
--- app/src/main/java/com/rafaelneves/anacosmeticos/ui/widget/ItemListCard.kt
+++ app/src/main/java/com/rafaelneves/anacosmeticos/ui/widget/ItemListCard.kt
@@ -27,6 +27,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -252,7 +253,7 @@ fun ProductBoxCardPreview() {
 fun PreviewIconTextArrowCard() {
     CardHomeScreen(
         icon = painterResource(id = R.drawable.ic_shipping),
-        text = "Enviados",
+        text = stringResource(id = R.string.shipping),
         onClick = {}
     )
 }
